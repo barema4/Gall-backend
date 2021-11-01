d02d222909b1386247288e62b642c5cad918a3e1
barema4 <samrubarema6@gmail.com>
2021-11-01 08:41:12 +0300

    initial backend data

---
+++ base/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from rest_framework import serializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.http import JsonResponse, response
+from .models import Product
+from .serializers import ProductSerializer
+# from .products import products
+
+# Create your views here.
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+
+    ]
+    return Response('hello')
+
+
+@api_view(['GET'])
+def getProducts(request):
+    products = Product.objects.all()
+    serializer =  ProductSerializer(products, many = True)
+    return Response(serializer.data)
+
+
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = None
+    for i in products:
+        if(i['_id'] == pk):
+            product = i
+            break
+
+    return Response(product)
+
+ 
+
+
+
